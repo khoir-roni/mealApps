c065b1f8f86793579d6379b18c0d20b4eeca8d81
khoir-roni <khoir.roni@gmail.com>
2021-11-22 16:51:10 +0700

    dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'DeliMeals',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: myHomePage(),
-  ));
-}
+void main() => runApp(MyApp());
 
-class myHomePage extends StatefulWidget {
-  myHomePage({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
-  _myHomePageState createState() => _myHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
 }
 
-class _myHomePageState extends State<myHomePage> {
+class MyHomePage extends StatefulWidget {
+    @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
